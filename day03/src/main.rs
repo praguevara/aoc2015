66e7ca58eaf4a80997a609cd02d2b3649997d356
Pablo Ramón Guevara <praguevara@gmail.com>
2023-12-05 22:00:25 +0100

    Day 03 - second star

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,32 +1,71 @@
+#![feature(iter_array_chunks)]
+
 use std::collections::HashSet;
 
-fn parse_input(input: &str) -> impl Iterator<Item = (i32, i32)> + '_ {
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+fn parse_input(input: &str) -> impl Iterator<Item = Coord> + '_ {
     input.chars().map(|c| match c {
-        '^' => (0, 1),
-        'v' => (0, -1),
-        '>' => (1, 0),
-        '<' => (-1, 0),
+        '^' => Coord { x: 0, y: 1 },
+        'v' => Coord { x: 0, y: -1 },
+        '>' => Coord { x: 1, y: 0 },
+        '<' => Coord { x: -1, y: 0 },
         _ => unreachable!(),
     })
 }
 
-fn unique_houses(directions: impl IntoIterator<Item = (i32, i32)>) -> usize {
+fn houses_visited(directions: impl IntoIterator<Item = Coord>) -> impl IntoIterator<Item = Coord> {
     let mut path = vec![];
-    let mut pos = (0, 0);
+    let mut pos = Coord::default();
 
     path.push(pos);
-    for (x, y) in directions {
-        pos.0 += x;
-        pos.1 += y;
+    for Coord { x, y } in directions {
+        pos.x += x;
+        pos.y += y;
         path.push(pos)
     }
+    path
+}
 
-    path.iter().collect::<HashSet<_>>().len()
+fn unique_houses(directions: impl IntoIterator<Item = Coord>) -> usize {
+    houses_visited(directions)
+        .into_iter()
+        .collect::<HashSet<_>>()
+        .len()
+}
+
+fn santa_and_robo_directions(
+    directions: impl IntoIterator<Item = Coord>,
+) -> (
+    impl IntoIterator<Item = Coord>,
+    impl IntoIterator<Item = Coord>,
+) {
+    directions
+        .into_iter()
+        .array_chunks::<2>()
+        .map(|[a, b]| (a, b))
+        .unzip::<_, _, Vec<_>, Vec<_>>()
+}
+
+fn unique_houses_with_robo_santa(directions: impl IntoIterator<Item = Coord>) -> usize {
+    let (santa_directions, robo_santa_directions) = santa_and_robo_directions(directions);
+    let santa_houses = houses_visited(santa_directions);
+    let robo_santa_houses = houses_visited(robo_santa_directions);
+
+    santa_houses
+        .into_iter()
+        .chain(robo_santa_houses)
+        .collect::<HashSet<_>>()
+        .len()
 }
 
 fn main() {
     let input = include_str!("../input.txt");
     let directions = parse_input(input);
 
-    println!("{}", unique_houses(directions));
+    println!("{}", unique_houses_with_robo_santa(directions));
 }
