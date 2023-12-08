6f98299cb307fb09cb9155c118d2615412b33cae
Pablo Ramón Guevara <praguevara@gmail.com>
2023-12-08 20:35:59 +0100

    Day 08 - first star

---
+++ day08/src/main.rs
@@ -0,0 +1,113 @@
+fn count_chars_literals_values(input: &str) -> (usize, usize) {
+    let mut literals = 0;
+    let mut values = 0;
+
+    let mut escaping = false;
+    let mut escaping_hex_remaining = 0;
+
+    for c in input.chars() {
+        match c {
+            '\\' => {
+                if escaping {
+                    // "\\"
+                    escaping = false;
+                    values += 1;
+                } else {
+                    // "\"
+                    escaping = true;
+                }
+            }
+            'x' => {
+                if escaping {
+                    if escaping_hex_remaining == 0 {
+                        // "\x"
+                        escaping_hex_remaining = 2;
+                    } else {
+                        // "\xx"
+                        escaping_hex_remaining -= 1;
+                        if escaping_hex_remaining == 0 {
+                            // "\xAx"
+                            escaping = false;
+                            values += 1;
+                        }
+                    }
+                } else {
+                    values += 1;
+                }
+            }
+            '\"' => {
+                if escaping {
+                    if escaping_hex_remaining > 0 {
+                        escaping_hex_remaining -= 1;
+                        if escaping_hex_remaining == 0 {
+                            // "\xA\""
+                            escaping = false;
+                            values += 1;
+                        }
+                    } else {
+                        // "\\"
+                        escaping = false;
+                        values += 1;
+                    }
+                }
+            }
+            _ => {
+                if escaping && escaping_hex_remaining > 0 {
+                    // "\xA"
+                    escaping_hex_remaining -= 1;
+                }
+                if escaping_hex_remaining == 0 {
+                    // "\xAA"
+                    escaping = false;
+                    values += 1;
+                }
+            }
+        }
+        literals += 1;
+    }
+
+    (literals, values)
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let lines = input.lines();
+    let (literals, values) = lines
+        .map(|line| {
+            let (a, b) = count_chars_literals_values(line);
+            (a, b)
+        })
+        .reduce(|(a, b), (c, d)| (a + c, b + d))
+        .unwrap();
+    println!("{}", literals - values);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    pub fn sample_input() -> Vec<&'static str> {
+        vec!["\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\""]
+    }
+
+    #[test]
+    fn test() {
+        assert_eq!(count_chars_literals_values("xxx"), (3, 3));
+    }
+
+    #[test]
+    fn test_count_chars_literals_values() {
+        let sample_input = test::sample_input();
+        let expected = [(2usize, 0usize), (5, 3), (10, 7), (6, 1)];
+        let actual: Vec<_> = sample_input
+            .iter()
+            .map(|s| count_chars_literals_values(s))
+            .collect();
+
+        expected
+            .iter()
+            .zip(actual)
+            .zip(sample_input)
+            .for_each(|((e, a), i)| assert_eq!(*e, a, "{}", i));
+    }
+}
